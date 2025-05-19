6ecc1eeae12abfbbb39049ec3e26c2d8c10d05dc
snov <s-n-o-v@yandex.ru>
2025-05-19 10:21:11 +0300

    feat: logout route

--- src/routes/api.php
+++ src/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', fn(Request $request) => $request->user());
